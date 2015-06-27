d8f4183da72a0caed8e87bc7f15e558e281c9d0c
Jim Witschey <jim.witschey@gmail.com>
2015-06-27 18:43:52 -0400

    initial commit

---
+++ pasteable/pasteable.py
@@ -0,0 +1,44 @@
+from __future__ import print_function
+from nose.plugins import Plugin
+from os.path import relpath
+from os import getcwd
+
+
+class Pasteable(Plugin):
+    """
+    Generates output about failing test cases in the same format as test
+    specifiers for the nosetests command. In other words:
+
+    $ nosetests
+    """
+    enabled = False
+    env_opt = 'NOSE_PASTEABLE'
+
+    def options(self, parser, env):
+        super(Pasteable, self).configure(parser, env)
+
+        parser.set_defaults(pasteable=env.get(self.env_opt, False))
+        pasteable_help = ('Generate reports with test name specifiers that '
+                          'you can paste back as nosetests input to rerun '
+                          'tests. '
+                          '[{env_opt}]'.format(env_opt=self.env_opt))
+        parser.add_option('--pasteable',
+                          dest='pasteable',
+                          action='store_true',
+                          help=pasteable_help)
+        no_pasteable_help = ('Override the {env_opt} environment '
+                             'variable.'.format(env_opt=self.env_opt))
+        parser.add_option('--no-pasteable',
+                          dest='pasteable',
+                          action='store_false',
+                          help=no_pasteable_help)
+
+    def configure(self, options, conf):
+        super(Pasteable, self).configure(options, conf)
+        self.enabled = options.pasteable
+
+    def testName(self, test):
+        test_file_absolute_path, _, test_object = test.address()
+        test_file_relative_path = relpath(test_file_absolute_path, getcwd())
+
+        return '{f}:{o}'.format(f=test_file_relative_path, o=test_object)
---
+++ test_script.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bats
+
+function grep_passed_tests() {
+  grep '\.\.\. ok'
+}
+function num_passed_tests() {
+  grep_passed_tests | wc -l
+}
+
+@test 'nosetests with the plugin loaded but disabled discovers the tests' {
+  [ `nosetests --collect-only --verbosity=2 2>&1 | grep_passed_tests | wc -l` -eq 6 ]
+}
+
+@test 'discovers the same tests, with different names, with pasteable enabled via env var' {
+  before_results=`NOSE_PASTEABLE='' nosetests --collect-only --verbosity=2 2>&1`
+  after_results=`NOSE_PASTEABLE='true' nosetests --collect-only --verbosity=2 2>&1`
+
+  [ `echo "$before_results" | num_passed_tests` -eq `echo "$after_results" | num_passed_tests` ]
+  [ "`echo $before_results`" != "`echo $after_results`" ]
+}
+
+@test 'output when enabled via --pasteable is the same as with env var' {
+  before_results=`NOSE_PASTEABLE='' nosetests --collect-only --pasteable --verbosity=2 2>&1`
+  after_results=`NOSE_PASTEABLE='true' nosetests --collect-only --verbosity=2 2>&1`
+  before_results="`echo "$before_results" | grep_passed_tests`"
+  after_results="`echo "$after_results" | grep_passed_tests`"
+
+  [ "$before_results" == "$after_results" ]
+}
+
+@test 'test names generated can be used to run tests via nosetests' {
+  valid_tests=`
+    nosetests --collect-only --verbosity=2 --pasteable 2>&1 |
+    grep_passed_tests |
+    awk '{print $1}'
+  `
+  for test_name in `echo "$valid_tests"`; do
+    echo "testing $test_name" 2>&1
+    [ `nosetests $test_name --verbosity=2 2>&1 | num_passed_tests` == 1 ]
+  done
+}

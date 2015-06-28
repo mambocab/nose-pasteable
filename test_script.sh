a07c3f13b9d9f09d41092398d22e31356bd6892e
Jim Witschey <jim.witschey@gmail.com>
2015-06-28 02:06:58 -0400

    much better test harness

--- test_script.sh
+++ test_script.sh
@@ -1,52 +1,41 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bats
 
-indent_level=0
-tab_width=2
-failures=''
-
-# first, the world's silliest rspec-style test "framework"
-function indent() {
-  case "$1" in
-    up)    indent_level=$(($indent_level + 1)) ;;
-    down)  indent_level=$(($indent_level - 1)) ;;
-    *)     exit 1
-  esac
+function grep_passed_tests() {
+  grep '\.\.\. ok'
 }
-function report() {
-  indent_string=$(
-    python -c "from __future__ import print_function ; print(' ' * ($indent_level * $tab_width))"
-  )
-  printf "$@" | sed -e "s/^/$indent_string/"
-}
-function context() {
-  report "$1\n"
-  indent up
-}
-function end_context() {
-  indent down
-}
-function it() {
-  report "$1 ... "
-  test_command="${@:2}"
-  if [ $test_command ] ; then
-    echo "\033[1;32mok\033[0m"
-  else
-    echo "\033[31mfailed\033[0m"
-    indent up
-    report "(evaluated \`$test_command\`)\n"
-    indent down
-  fi
+function num_passed_tests() {
+  grep_passed_tests | wc -l
 }
 
-context 'nosetests without pasteable'
-  before_results=`NOSE_PASTEABLE='' nosetests --collect-only --verbosity=2 2>&1`
-  it 'discovers the correct number of tests' `echo "$before_results" | grep '\.\.\. ok' | wc -l` -eq 6
-end_context
+@test 'nosetests with the plugin loaded but disabled discovers the tests' {
+  [ `nosetests --collect-only --verbosity=2 2>&1 | grep_passed_tests | wc -l` -eq 6 ]
+}
 
-context 'NOSE_PASTEABLE environment variable'
+@test 'discovers the same tests, with different names, with pasteable enabled via env var' {
   before_results=`NOSE_PASTEABLE='' nosetests --collect-only --verbosity=2 2>&1`
   after_results=`NOSE_PASTEABLE='true' nosetests --collect-only --verbosity=2 2>&1`
 
-  it 'discovers the same number of tests' `echo "$before_results" | wc -l` -eq `echo "$after_results" | wc -l`
-  it 'produces a different result' "'$before_results'" = "'$after_results'"
-end_context
+  [ `echo "$before_results" | num_passed_tests` -eq `echo "$after_results" | num_passed_tests` ]
+  [ "`echo $before_results`" != "`echo $after_results`" ]
+}
+
+@test 'output when enabled via --pasteable is the same as with env var' {
+  before_results=`NOSE_PASTEABLE='' nosetests --collect-only --pasteable --verbosity=2 2>&1`
+  after_results=`NOSE_PASTEABLE='true' nosetests --collect-only --verbosity=2 2>&1`
+  before_results="`echo "$before_results" | grep_passed_tests`"
+  after_results="`echo "$after_results" | grep_passed_tests`"
+
+  [ "$before_results" == "$after_results" ]
+}
+
+@test 'test names generated can be used to run tests via nosetests' {
+  valid_tests=`
+    nosetests --collect-only --verbosity=2 --pasteable 2>&1 |
+    grep_passed_tests |
+    awk '{print $1}'
+  `
+  for test_name in `echo "$valid_tests"`; do
+    echo "testing $test_name" 2>&1
+    [ `nosetests $test_name --verbosity=2 2>&1 | num_passed_tests` == 1 ]
+  done
+}

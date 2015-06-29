343ca43e04042e92fc0b693cde115ea8245d30ca
Jim Witschey <jim.witschey@gmail.com>
2015-06-29 11:10:51 -0400

    more info in setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='Nose-Pasteable',
+    name='nose-pasteable',
     version='0.1',
     packages=find_packages(),
-
+    author='Jim Witschey',
+    author_email='jim.witschey@gmail.com',
+    url='https://github.com/mambocab/nose-pasteable',
+    license='MIT',
     entry_points={
         'nose.plugins': ['pasteable = pasteable.pasteable:Pasteable']
     },

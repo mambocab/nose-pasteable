80d2a4cdb653ee8dc040ad4590786ecdc816aa3e
Jim Witschey <jim.witschey@gmail.com>
2015-06-29 21:46:09 -0400

    bump version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='nose-pasteable',
-    version='0.1',
+    version='0.2',
     packages=find_packages(),
     author='Jim Witschey',
     author_email='jim.witschey@gmail.com',
